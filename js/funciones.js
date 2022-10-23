82211356da3be4904b4ac765e766e5b8356b3bc7
Berlin <alejandro19rubio88@gmail.com>
2022-10-23 15:48:25 -0500

    index update

--- js/funciones.js
+++ js/funciones.js
@@ -14,8 +14,10 @@ const enviarFormulario =() => {
     let numero = 573016677664
 var win= window.open ("https://api.whatsapp.com/send?phone=573163745958&text=%C2%A1Hola%21+Deseo+informaci%C3%B3n+de+sus+servicios%2C+me+comunico+desde+la+ciudad+de&type=phone_number&app_absent=0")
 }
+/*
 evento.addEventListener('click', enviarFormulario)
 
 "https://api.whatsapp.com/send?phone=573163745958&text=%C2%A1Hola%21+Deseo+informaci%C3%B3n+de+sus+servicios%2C+me+comunico+desde+la+ciudad+de&type=phone_number&app_absent=0"
 
 `https://api.whatsapp.com/send?phone=${numero}&text=Hola%20mi%20nombre%20es%20${nombre}%20,Asunto%20${mensaje}%20me%20puede%20contactar%20al%20${celular}`)
+*/
